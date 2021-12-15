41f1ad0874ff5780ba8201450bb400a408e00438
HwangSangWook <goodbyeyo@naver.com>
2021-12-15 23:24:04 +0900

    RequestMapping URL 중복 통합

--- src/main/java/study/servlet/web/springmvc/v2/SpringMemberControllerV2.java
+++ src/main/java/study/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -12,16 +12,17 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/springmvc/v2/members")
 public class SpringMemberControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/springmvc/v2/members/new-form")
+    @RequestMapping("/new-form")
     public ModelAndView newForm() {
         return new ModelAndView("new-form");
     }
 
-    @RequestMapping("/springmvc/v2/members/save")
+    @RequestMapping("/save")
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
@@ -34,8 +35,8 @@ public class SpringMemberControllerV2 {
 
         return mv;
     }
-
-    @RequestMapping("/springmvc/v2/members")
+    // /springmvc/v2/members
+    @RequestMapping
     public ModelAndView members(Map<String, String> paramMap) {
         List<Member> members = memberRepository.findAll();
         ModelAndView mv = new ModelAndView("members");
